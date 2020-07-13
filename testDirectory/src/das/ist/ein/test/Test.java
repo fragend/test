e25eee2b64da6275c6479f200eddbea3a73897da
christian <christianweisich3@web.de>
2020-07-13 19:25:54 +0200

    Hallo neuer test

--- testDirectory/src/das/ist/ein/test/Test.java
+++ testDirectory/src/das/ist/ein/test/Test.java
@@ -1,8 +1,11 @@
 package das.ist.ein.test;
 
+
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Moin");
+		System.out.println("HALLLLO");
 	}
 }
